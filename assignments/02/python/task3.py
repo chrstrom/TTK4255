ede7b8ac9dd3e8583100518257e4345323c9b23e
Christopher Strøm <detoxte@gmail.com>
2022-01-24 12:56:07 +0100

    format handout code
    Signed-off-by: Christopher Strøm <detoxte@gmail.com>

--- assignments/02/python/task3.py
+++ assignments/02/python/task3.py
@@ -5,7 +5,7 @@ import numpy as np
 from common import *
 
 # Note: the sample image is naturally grayscale
-I = rgb_to_gray(im2double(plt.imread('../data/calibration.jpg')))
+I = rgb_to_gray(im2double(plt.imread("../data/calibration.jpg")))
 
 ###########################################
 #
@@ -15,24 +15,24 @@ I = rgb_to_gray(im2double(plt.imread('../data/calibration.jpg')))
 sigma_D = 1
 sigma_I = 3
 alpha = 0.06
-response = np.zeros_like(I) # Placeholder
+response = np.zeros_like(I)  # Placeholder
 
 ###########################################
 #
 # Task 3.4: Extract local maxima
 #
 ###########################################
-corners_y = [0] # Placeholder
-corners_x = [0] # Placeholder
+corners_y = [0]  # Placeholder
+corners_x = [0]  # Placeholder
 
 ###########################################
 #
 # Figure 3.1: Display Harris-Stephens corner strength
 #
 ###########################################
-plt.figure(figsize=(13,5))
+plt.figure(figsize=(13, 5))
 plt.imshow(response)
-plt.colorbar(label='Corner strength')
+plt.colorbar(label="Corner strength")
 plt.tight_layout()
 # plt.savefig('out_corner_strength.png', bbox_inches='tight', pad_inches=0) # Uncomment to save figure in working directory
 
@@ -41,9 +41,9 @@ plt.tight_layout()
 # Figure 3.4: Display extracted corners
 #
 ###########################################
-plt.figure(figsize=(10,5))
-plt.imshow(I, cmap='gray')
-plt.scatter(corners_x, corners_y, linewidths=1, edgecolor='black', color='yellow', s=9)
+plt.figure(figsize=(10, 5))
+plt.imshow(I, cmap="gray")
+plt.scatter(corners_x, corners_y, linewidths=1, edgecolor="black", color="yellow", s=9)
 plt.tight_layout()
 # plt.savefig('out_corners.png', bbox_inches='tight', pad_inches=0) # Uncomment to save figure in working directory
 
