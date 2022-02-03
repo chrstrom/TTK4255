adc23af784ed27eb3b9c5e65f3a2199f16f46438
Christopher Strøm <detoxte@gmail.com>
2022-02-03 18:01:32 +0100

    cleanup imports
    Signed-off-by: Christopher Strøm <detoxte@gmail.com>

--- assignments/03/python/task4.py
+++ assignments/03/python/task4.py
@@ -4,7 +4,7 @@ from fnmatch import translate
 from turtle import xcor
 import numpy as np
 import matplotlib.pyplot as plt
-from common import *
+import common as cm
 
 helicopter_image = plt.imread("../data/quanser.jpg")
 
@@ -20,37 +20,36 @@ T_from_platform_to_camera = np.loadtxt("../data/platform_to_camera.txt")
 # All screws are placed with a distance d from the others
 
 d = 0.1145
-x0 = np.array([0, 0, 0, 1])
-x1 = np.array([d, 0, 0, 1])
-x2 = np.array([d, d, 0, 1])
-x3 = np.array([0, d, 0, 1])
-
 axis_scale = 0.05
 psi = np.deg2rad(11.6)
 theta = np.deg2rad(28.9)
 phi = np.deg2rad(0)
 
 # 4.2
+x0 = np.array([0, 0, 0, 1])
+x1 = np.array([d, 0, 0, 1])
+x2 = np.array([d, d, 0, 1])
+x3 = np.array([0, d, 0, 1])
 X_p = np.c_[x0, x1, x2, x3].reshape(4, 4)
 X_c = T_from_platform_to_camera @ X_p
-u, v = project(K, X_c)
+u, v = cm.project(K, X_c)
 
 # 4.3
 T_from_base_to_camera = (
-    T_from_platform_to_camera @ translate_xyz(x=d / 2, y=d / 2) @ rotate_z(psi)
+    T_from_platform_to_camera @ cm.translate_xyz(x=d / 2, y=d / 2) @ cm.rotate_z(psi)
 )
 
 # 4.4
 T_from_hinge_to_camera = (
-    T_from_base_to_camera @ translate_xyz(z=0.325) @ rotate_y(theta)
+    T_from_base_to_camera @ cm.translate_xyz(z=0.325) @ cm.rotate_y(theta)
 )
 
 # 4.5
-T_from_arm_to_camera = T_from_hinge_to_camera @ translate_xyz(z=-0.05)
+T_from_arm_to_camera = T_from_hinge_to_camera @ cm.translate_xyz(z=-0.05)
 
 # 4.6
 T_from_rotors_to_camera = (
-    T_from_arm_to_camera @ translate_xyz(x=0.65, z=-0.03) @ rotate_z(phi)
+    T_from_arm_to_camera @ cm.translate_xyz(x=0.65, z=-0.03) @ cm.rotate_z(phi)
 )
 
 # 4.7
@@ -58,16 +57,16 @@ marker_points = np.loadtxt("../data/heli_points.txt")
 markers_in_arm_frame = marker_points[:3, :]
 markers_in_rotor_frame = marker_points[3:, :]
 
-u_arm, v_arm = project(K, T_from_arm_to_camera @ markers_in_arm_frame.T)
-u_rotor, v_rotor = project(K, T_from_rotors_to_camera @ markers_in_rotor_frame.T)
+u_arm, v_arm = cm.project(K, T_from_arm_to_camera @ markers_in_arm_frame.T)
+u_rotor, v_rotor = cm.project(K, T_from_rotors_to_camera @ markers_in_rotor_frame.T)
 
 plt.figure()
 plt.imshow(helicopter_image)
-draw_frame(K, T_from_platform_to_camera, scale=axis_scale)
-draw_frame(K, T_from_base_to_camera, scale=axis_scale)
-draw_frame(K, T_from_hinge_to_camera, scale=axis_scale)
-draw_frame(K, T_from_arm_to_camera, scale=axis_scale)
-draw_frame(K, T_from_rotors_to_camera, scale=axis_scale)
+cm.draw_frame(K, T_from_platform_to_camera, scale=axis_scale)
+cm.draw_frame(K, T_from_base_to_camera, scale=axis_scale)
+cm.draw_frame(K, T_from_hinge_to_camera, scale=axis_scale)
+cm.draw_frame(K, T_from_arm_to_camera, scale=axis_scale)
+cm.draw_frame(K, T_from_rotors_to_camera, scale=axis_scale)
 plt.scatter(u, v, c="yellow", marker="x", s=100)
 plt.scatter(u_arm, v_arm, c="yellow", marker=".", s=100)
 plt.scatter(u_rotor, v_rotor, c="yellow", marker=".", s=100)
