0fbce99fb65c2fd28a3b138182603ed04e35be6b
Christopher Strøm <detoxte@gmail.com>
2022-02-28 19:41:07 +0100

    finish first part of task 2
    Signed-off-by: Christopher Strøm <detoxte@gmail.com>

--- midterm_project/python/part2.py
+++ midterm_project/python/part2.py
@@ -1,16 +1,68 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from common import *
+from scipy.optimize import least_squares
 
 # Tip: The solution from HW4 is inside common.py
 
 K = np.loadtxt('../data/K.txt')
-u = np.loadtxt('../data/platform_corners_image.txt')
-X = np.loadtxt('../data/platform_corners_metric.txt')
-I = plt.imread('../data/video0000.jpg') # Only used for plotting
+u = np.loadtxt('../data/platform_corners_image.txt') #each column is (ui, vi).T 
+X = np.loadtxt('../data/platform_corners_metric.txt') # each column is (X Y 0 1).T
+I = plt.imread('../data/img_sequence/video0000.jpg') # Only used for plotting
 
-# Example: Compute predicted image locations and reprojection errors
-T_hat = translate(-0.3, 0.1, 1.0)@rotate_x(1.8)
+# Task 2.1 (from HW4)
+uv1 = np.vstack((u, np.ones(u.shape[1])))
+xy_tilde = np.linalg.inv(K) @ uv1
+xy = xy_tilde / xy_tilde[2,:]
+xy = xy_tilde[:2, :]
+XY = X[:2, :]
+
+H = estimate_H(xy, XY)
+
+# (a)
+# XY1 = np.vstack([X[:2, :], X[3, :]])
+# T_hat = H
+# u_hat = project(K, H@XY1)
+
+# (b)
+# T1, T2 = decompose_H(H)
+# translation_z = T1[2, 3]
+# T_hat = T1 if translation_z > 0 else T2
+# u_hat = project(K, T_hat@X)
+# errors = np.linalg.norm(u - u_hat, axis=0)
+
+
+
+# Task 2.3
+# Parametrized as p0, p1, p2, t0 t1 t2
+h = np.array([0.1, 0.1, 0.1, 0.1, 0.1, 0.1])
+def T_from_h(h):
+    p = h[:3]
+    t = h[3:]
+    R = rotate_x(p[0])@rotate_y(p[1])@rotate_z(p[2])
+
+    T = np.eye(4)
+    T[:3, :3] = R[:3, :3]
+    T[:3, 3] = t
+    return T
+
+def resfun(h):
+    T = T_from_h(h)
+    u_hat = project(K, T@X)
+    N = u.shape[0]
+    M = u.shape[1]
+    r = np.zeros(N*M)
+    for i in range(M):
+        r[i] = u_hat[0, i] - u[0, i]
+        r[i+M] = u_hat[1, i] - u[1, i]
+
+    return r
+
+
+# Tip: Use the previous image's parameter estimate as initialization
+h = least_squares(resfun, x0=h, method='lm').x
+
+T_hat = T_from_h(h)
 u_hat = project(K, T_hat@X)
 errors = np.linalg.norm(u - u_hat, axis=0)
 
@@ -40,6 +92,6 @@ plt.ylim([600, 350])
 # plt.ylim([I.shape[0], 0])
 
 # Tip: To save the figure:
-# plt.savefig('out_part2.png')
+#plt.savefig('out_part2.1_b.png')
 
 plt.show()
