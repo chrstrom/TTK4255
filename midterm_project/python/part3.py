ae7250e14a39d0b760a9bc32c9be945eefd37069
Christopher Strøm <detoxte@gmail.com>
2022-03-01 13:32:01 +0100

    refactor with classes
    Signed-off-by: Christopher Strøm <detoxte@gmail.com>

--- midterm_project/python/part3.py
+++ midterm_project/python/part3.py
@@ -7,99 +7,121 @@ from common import translate, rotate_x, rotate_y, rotate_z, project
 import part1b
 
 
-def T_hat_model_A(kinematic_parameters, rpy):
-    T_base_platform = translate(
-        kinematic_parameters[0] / 2, kinematic_parameters[0] / 2, 0.0
-    ) @ rotate_z(rpy[0])
-    T_hinge_base = translate(0.0, 0.0, kinematic_parameters[1]) @ rotate_y(rpy[1])
-    T_arm_hinge = translate(0.0, 0.0, -kinematic_parameters[2])
-    T_rotors_arm = translate(
-        kinematic_parameters[3], 0.0, -kinematic_parameters[4]
-    ) @ rotate_x(rpy[2])
+class kinematic_model_A:
+    def __init__(self):
 
-    T_base_camera = T_platform_camera @ T_base_platform
-    T_hinge_camera = T_base_camera @ T_hinge_base
-    T_arm_camera = T_hinge_camera @ T_arm_hinge
-    T_rotors_camera = T_arm_camera @ T_rotors_arm
+        self.T_platform_camera = np.loadtxt("../data/platform_to_camera.txt")
+        self.detections = np.loadtxt("../data/detections.txt")
+        self.K = np.loadtxt("../data/K.txt")
 
-    return T_rotors_camera, T_arm_camera
+        self.N = self.detections.shape[0]
+        self.M = 7
+
+        initial_lengths = np.array(
+            [0.1, 0.1, 0.1, 0.1, 0.1]
+        )  # actual values: 0.1145, 0.325, 0.050, 0.65, 0.030
+        initial_markers = 0.1 * np.ones(21)  # actual values in heli_points[:3, :]
+        self.initial_parameters = np.hstack((initial_lengths, initial_markers))
+        self.KP = self.initial_parameters.shape[0]
+
+        A1 = np.ones([2 * self.M * self.N, self.KP])
+        B = np.ones([2 * self.M, 3])
+        A2 = B.copy()
+        for _ in range(self.N - 1):
+            A2 = block_diag(A2, B)
+        self.JS = np.block([A1, A2])
+
+    def T_hat(self, kinematic_parameters, rpy):
+        T_base_platform = translate(
+            kinematic_parameters[0] / 2, kinematic_parameters[0] / 2, 0.0
+        ) @ rotate_z(rpy[0])
+        T_hinge_base = translate(0.0, 0.0, kinematic_parameters[1]) @ rotate_y(rpy[1])
+        T_arm_hinge = translate(0.0, 0.0, -kinematic_parameters[2])
+        T_rotors_arm = translate(
+            kinematic_parameters[3], 0.0, -kinematic_parameters[4]
+        ) @ rotate_x(rpy[2])
+
+        T_base_camera = self.T_platform_camera @ T_base_platform
+        T_hinge_camera = T_base_camera @ T_hinge_base
+        T_arm_camera = T_hinge_camera @ T_arm_hinge
+        T_rotors_camera = T_arm_camera @ T_rotors_arm
+
+        return T_rotors_camera, T_arm_camera
 
 
-def u_hat(kinematic_parameters, state):
-    marker_points = kinematic_parameters[-3 * M :].reshape((3, M))
-    marker_points = np.vstack((marker_points, np.ones((1, M))))
+class BatchOptimizer:
+    def __init__(self, model):
+        self.model = model
 
-    T_rotors_camera, T_arm_camera = T_hat_model_A(kinematic_parameters, state)
+        initial_state_parameters, _ = part1b.detected_trajectory()
+        initial_state_parameters = initial_state_parameters.flatten()
+        self.initial_parameters = np.hstack(
+            [kinematic_model.initial_parameters, initial_state_parameters]
+        )
 
-    markers_rotor = T_rotors_camera @ marker_points[:, 3:]
-    markers_arm = T_arm_camera @ marker_points[:, :3]
+    def u_hat(self, kinematic_parameters, state):
+        marker_points = kinematic_parameters[-3 * self.model.M :].reshape(
+            (3, self.model.M)
+        )
+        marker_points = np.vstack((marker_points, np.ones((1, self.model.M))))
 
-    X = np.hstack([markers_arm, markers_rotor])
-    u = project(K, X)
-    return u
+        T_rotors_camera, T_arm_camera = self.model.T_hat(kinematic_parameters, state)
 
+        markers_rotor = T_rotors_camera @ marker_points[:, 3:]
+        markers_arm = T_arm_camera @ marker_points[:, :3]
 
-def residuals(p):
-    kinematic_parameters = p[:KP]
-    state_parameters = p[KP:]
+        X = np.hstack([markers_arm, markers_rotor])
+        u = project(self.model.K, X)
+        return u
 
-    r = np.zeros(2 * M * N)
-    for i in range(N):
-        ui = detections[i, 1::3]
-        vi = detections[i, 2::3]
-        weights = detections[i, ::3]
-        u = np.vstack((ui, vi))
+    def residuals(self, p):
+        kinematic_parameters = p[: self.model.KP]
+        state_parameters = p[self.model.KP :]
 
-        state = state_parameters[3 * i : 3 * (i + 1)]
+        r = np.zeros(2 * self.model.M * self.model.N)
+        for i in range(self.model.N):
+            ui = self.model.detections[i, 1::3]
+            vi = self.model.detections[i, 2::3]
+            weights = self.model.detections[i, ::3]
+            u = np.vstack((ui, vi))
 
-        r[2 * M * i : 2 * M * (i + 1)] = (
-            weights * (u_hat(kinematic_parameters, state) - u)
-        ).flatten()
+            state = state_parameters[3 * i : 3 * (i + 1)]
 
-    return r
+            r[2 * self.model.M * i : 2 * self.model.M * (i + 1)] = (
+                weights * (self.u_hat(kinematic_parameters, state) - u)
+            ).flatten()
+
+        return r
+
+    def optimize(self):
+
+        optimized_parameters = least_squares(
+            self.residuals,
+            self.initial_parameters,
+            xtol=1e-6,
+            jac_sparsity=self.model.JS,
+        )
+
+        return optimized_parameters.x
 
 
 if __name__ == "__main__":
 
-    detections = np.loadtxt("../data/detections.txt")
-    heli_points = np.loadtxt("../data/heli_points.txt").T
-    K = np.loadtxt("../data/K.txt")
-    T_platform_camera = np.loadtxt("../data/platform_to_camera.txt")
+    kinematic_model = kinematic_model_A()
 
-    initial_lengths = np.array([0.1145, 0.325, 0.050, 0.65, 0.030])
-    initial_markers = heli_points[:3, :].flatten()
-    initial_kinematic_parameters = np.hstack((initial_lengths, initial_markers))
+    optimizer = BatchOptimizer(kinematic_model)
+    optimized_parameters = optimizer.optimize()
 
-    initial_state_parameters, _ = part1b.detected_trajectory()
-    initial_state_parameters = initial_state_parameters.flatten()
-    initial_parameters = np.hstack(
-        [initial_kinematic_parameters, initial_state_parameters]
-    )
-
-    N = detections.shape[0]
-    KP = initial_kinematic_parameters.shape[0]
-    M = 7
-
-    A1 = np.ones([2 * M * N, KP])
-    B = np.ones([2 * M, 3])
-    A2 = B.copy()
-    for i in range(N - 1):
-        A2 = block_diag(A2, B)
-    JS = np.block([A1, A2])
-
-    optimized_parameters = least_squares(
-        residuals, initial_parameters, xtol=1e-4, jac_sparsity=JS
-    ).x
-
-    image_number = 125
+    image_number = 0
     do_plot = True
     if do_plot:
+        KP = kinematic_model.KP
         kinematic_parameters = optimized_parameters[:KP]
         rpy = optimized_parameters[KP + image_number * 3 : KP + (image_number + 1) * 3]
 
         plt.imshow(plt.imread("../data/img_sequence/video%04d.jpg" % image_number))
         plt.scatter(
-            *u_hat(kinematic_parameters, rpy),
+            *optimizer.u_hat(kinematic_parameters, rpy),
             linewidths=1,
             color="yellow",
             s=10,
