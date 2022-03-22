94b88b0769196de5e2ac497958ac253dbb3170c4
Christopher Strøm <detoxte@gmail.com>
2022-03-22 15:40:47 +0100

    Reestimate E with the inlier set and use prosac trials
    Signed-off-by: Christopher Strøm <detoxte@gmail.com>

--- assignments/05/python/estimate_E_ransac.py
+++ assignments/05/python/estimate_E_ransac.py
@@ -8,9 +8,10 @@ def estimate_E_ransac(xy1, xy2, uv1, uv2, K, distance_threshold, num_trials):
     m = 8
     max_inlier_count = 0
     E_best = -1
-    inlier_set = 0
+    inlier_set_best = 0
+
     for i in range(num_trials):
-        print(f"Iteration {i} of {num_trials}")
+        print(f"Iteration {i + 1} of {num_trials}")
         sample = np.random.choice(xy1.shape[1], size=m, replace=False)
         E = estimate_E(xy1[:,sample], xy2[:,sample])
         residuals = epipolar_distance(F_from_E(K, E), uv1, uv2)
--- assignments/05/python/main.py
+++ assignments/05/python/main.py
@@ -29,7 +29,9 @@ xy2 = project(K_inv, uv2)
 
 # # Task 2: Estimate E
 E = estimate_E(xy1, xy2)
-print(E)
+
+
+
 # F = F_from_E(E, K)
 
 # # Task 3: Triangulate 3D points
@@ -62,17 +64,49 @@ uv2 = np.vstack([matches[:, 2:4].T, np.ones(matches.shape[0])])
 xy1 = project(K_inv, uv1)
 xy2 = project(K_inv, uv2)
 
-E, inlier_set = estimate_E_ransac(xy1, xy2, uv1, uv2, K, 4, 2000)
-print(E)
+inlier_fraction = 0.5 
+success_probability = 0.99
+num_trials = np.ceil(np.log(1 - success_probability) / np.log(1 - inlier_fraction**8))
+
+E, inlier_set = estimate_E_ransac(xy1, xy2, uv1, uv2, K, 4, int(num_trials))
+
+inliers = np.where(inlier_set == True)[0]
+inlier_count = len(inliers)
+uv1_in = np.zeros((3, inlier_count))
+uv2_in = np.zeros((3, inlier_count))
+xy1_in = np.zeros((2, inlier_count))
+xy2_in = np.zeros((2, inlier_count))
+for i, inlier in enumerate(inliers):
+    uv1_in[:, i] = uv1[:, inlier]
+    uv2_in[:, i] = uv2[:, inlier]
+    xy1_in[:, i] = xy1[:, inlier]
+    xy2_in[:, i] = xy2[:, inlier]
+
+E = estimate_E(xy1_in, xy2_in)
 F = F_from_E(E, K)
 
-# Task 3: Triangulate 3D points
+#sample_size = 8 if inlier_count > 8 else inlier_count 
+sample_size = inlier_count
 P1 = np.c_[np.eye(3), np.zeros(3)]
-P2 = decompose_E(E)[0]
+# Select P2 for the view that is in front of both cameras
 
-X = triangulate_many(xy1, xy2, P1, P2)
-draw_correspondences(I1, I2, uv1, uv2, F, sample_size=8)
-draw_point_cloud(X, I1, uv1, xlim=[-1, +1], ylim=[-1, +1], zlim=[1, 3])
+max_points_in_front = 0
+i_best = 0
 
+# Iterate over all of the possible matrices and find the matrix with most
+# measurements in front of the camera
+P2s = decompose_E(E)
+for i, P in enumerate(P2s):
+    X = triangulate_many(xy1_in, xy2_in, P1, P)
 
+    points_in_front = np.sum((P1 @ X)[2] > 0)
+    if points_in_front > max_points_in_front:
+        max_points_in_front = points_in_front
+        i_best = i
+
+P2 = P2s[i_best]
+
+X = triangulate_many(xy1_in, xy2_in, P1, P2)
+draw_correspondences(I1, I2, uv1_in, uv2_in, F, sample_size=sample_size)
+draw_point_cloud(X, I1, uv1_in, xlim=[-1, +1], ylim=[-1, +1], zlim=[1, 3])
 plt.show()
