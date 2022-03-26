b8a46fccefa5a31d1c6f656b9d15629ef7369520
Christopher Strøm <detoxte@gmail.com>
2022-03-26 20:57:18 +0100

    add ensemble run of RANSAC
    Signed-off-by: Christopher Strøm <detoxte@gmail.com>

--- assignments/05/python/main.py
+++ assignments/05/python/main.py
@@ -5,9 +5,8 @@ from estimate_E import *
 from decompose_E import *
 from triangulate_many import *
 from epipolar_distance import *
-from F_from_E import *
 from estimate_E_ransac import *
-
+from datetime import datetime
 
 def project(K, X):
     X_tilde = K @ X
@@ -31,7 +30,7 @@ xy2 = project(K_inv, uv2)
 E = estimate_E(xy1, xy2)
 
 
-# F = F_from_E(E, K)
+# F = K_inv.T @ E @ K_inv
 
 # # Task 3: Triangulate 3D points
 # P1 = np.c_[np.eye(3), np.zeros(3)]
@@ -63,41 +62,48 @@ uv2 = np.vstack([matches[:, 2:4].T, np.ones(matches.shape[0])])
 xy1 = project(K_inv, uv1)
 xy2 = project(K_inv, uv2)
 
+distance_threshold = 4
 inlier_fraction = 0.5
 success_probability = 0.99
 num_trials = np.ceil(np.log(1 - success_probability) / np.log(1 - inlier_fraction ** 8))
 
-E, inlier_set = estimate_E_ransac(xy1, xy2, uv1, uv2, K, 4, int(50))
+time_start = datetime.now()
+inlier_set = estimate_E_ransac(xy1, xy2, uv1, uv2, K, distance_threshold, int(num_trials))
+print(f"Time taken: {datetime.now() - time_start}")
 
-inliers = np.where(inlier_set == True)[0]
+# total_inliers = 0
+# num_ensemble = 100
+# for i in range(num_ensemble):
+#     time_start = datetime.now()
+#     inlier_set = estimate_E_ransac(xy1, xy2, uv1, uv2, K, distance_threshold, int(num_trials))
+#     total_inliers = total_inliers + len(inlier_set)
+#     print(f"Iteration {i + 1} of {num_ensemble} ({datetime.now() - time_start})")
+# # print(f"Time taken: {datetime.now() - time_start}")
 
-uv1_in = uv1[:, inliers]
-uv2_in = uv2[:, inliers]
-xy1_in = xy1[:, inliers]
-xy2_in = xy2[:, inliers]
+# print(f"{num_ensemble}The  runs yielded an average inlier set size of {total_inliers / num_ensemble}")
 
-E = estimate_E(xy1_in, xy2_in)
-F = F_from_E(E, K)
+uv1_in = uv1[:, inlier_set]
+xy1_in = xy1[:, inlier_set]
+xy2_in = xy2[:, inlier_set]
 
-# sample_size = 8 if inlier_count > 8 else inlier_count
+E = estimate_E(xy1_in, xy2_in).reshape((3, 3))
+
 P1 = np.c_[np.eye(3), np.zeros(3)]
 
 # Select P2 for the view that is in front of both cameras
 max_points_in_front = 0
 i_best = 0
-
 P2s = decompose_E(E)
 for i, P in enumerate(P2s):
     X = triangulate_many(xy1_in, xy2_in, P1, P)
 
-    points_in_front = np.sum((P1 @ X)[2] > 0)
+    points_in_front = np.sum(X[2, :] > 0)
     if points_in_front > max_points_in_front:
         max_points_in_front = points_in_front
         i_best = i
 
 P2 = P2s[i_best]
 
-X = triangulate_many(xy1, xy2, P1, P2)
-# draw_correspondences(I1, I2, uv1_in, uv2_in, F, sample_size=sample_size)
-draw_point_cloud(X, I1, uv1, xlim=[-1, +1], ylim=[-1, +1], zlim=[1, 3])
+X = triangulate_many(xy1_in, xy2_in, P1, P2)
+draw_point_cloud(X, I1, uv1_in, xlim=[-1, +1], ylim=[-1, +1], zlim=[1, 3])
 plt.show()
