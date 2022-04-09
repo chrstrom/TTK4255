1a7b2e713e8fbe5ac05c5c103dd189f5d75f06f3
Christopher Strøm <detoxte@gmail.com>
2022-04-09 19:44:28 +0200

    add script and data for task 1.2
    Signed-off-by: Christopher Strøm <detoxte@gmail.com>

--- final_project/python/assess_uncertainty.py
+++ final_project/python/assess_uncertainty.py
@@ -25,4 +25,31 @@ import numpy as np
 import cv2 as cv
 
 if __name__ == '__main__':
-    pass
+    N = 10
+
+    image = "../data/calibration/checkerboard.png"
+    
+
+    camera_matrix = np.array(((2359.40946, 0, 1370.05852), (0, 2359.61091, 1059.63818), (0, 0, 1)))
+
+    for i in range(N):
+            
+        k1 = np.random.normal(-0.06652, 0.00109)
+        k2 = np.random.normal(0.06534, 0.00624)
+        k3 = np.random.normal(-0.07555, 0.01126)
+        p1 = np.random.normal(0.00065, 0.00011)
+        p2 = np.random.normal(-0.00419, 0.00014)
+
+        distortion_coefficients = np.array((k1, k2, k3, p1, p2))
+
+        I = cv.imread(image, cv.IMREAD_GRAYSCALE)
+
+        dst = cv.undistort(I, camera_matrix, distortion_coefficients, camera_matrix)
+
+        # cv.imshow("distorted image with sampled distortion coefficients", dst)
+        # cv.waitKey()
+
+        cv.imwrite(f"../data/distortion_sample/sample{i:03d}.png", dst) 
+
+
+
