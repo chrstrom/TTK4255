c3736c882e6a8167f09aa91c37be1b9aabb3adbd
Christopher Strøm <detoxte@gmail.com>
2022-04-11 21:23:40 +0200

    add skeleton for 3.1
    Signed-off-by: Christopher Strøm <detoxte@gmail.com>

---
+++ final_project/python/localize_camera.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+class LocalizeCamera():
+    """
+    Goal: localize camera in arbitrary image
+    Use show_localization_results.py to test
+    """
+
+    def __init__(self, image):
+        pass
+
+
+if __name__ == "__main__":
+    pass
